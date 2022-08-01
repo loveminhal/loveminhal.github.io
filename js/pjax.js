823aba6ef16a717e2c9e43bfe798746bd89c0549
loveminhal <m@minhal.me>
2022-08-02 00:22:47 +0800

    Site updated: 2022-08-02 00:22:47

--- js/pjax.js
+++ js/pjax.js
@@ -1,18 +1,1 @@
-function initPjax() {
-  new Pjax({
-    selectors: ["title", ".js-Pjax", "main", "aside"],
-  });
-}
-
-// for sidebar
-function isHome() {
-  if (window.location.pathname === CONFIG.root) {
-    document.body.classList.add("is-home");
-  } else {
-    document.body.classList.remove("is-home");
-  }
-}
-
-document.addEventListener("DOMContentLoaded", initPjax);
-document.addEventListener("DOMContentLoaded", isHome);
-document.addEventListener("pjax:success", isHome);
+import{b as e,f as n}from"./chunk-72ZP56JR.js";import"./chunk-FEIY7W7S.js";function t(){new window.Pjax({selectors:["title",".js-Pjax","main","aside"]})}function d(){e(),n()}document.addEventListener("DOMContentLoaded",t);document.addEventListener("DOMContentLoaded",e);document.addEventListener("pjax:success",d);
